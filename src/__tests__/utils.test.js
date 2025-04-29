45ce2bdd9991b769ba751b9df04e49b0154663bb
WADE <MEANTOINE12@GMAIL.COM>
2025-04-30 01:48:55 +0300

    complete

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome",()=>{
+    it("accepts a string as an argument and returns true if the string is a palindrome, and false if it isn't",()=>{
+        const word = "racecar"
+
+        const string = isPalindrome("racecar")
+
+        expect(string).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
 // Your code here
+export function isPalindrome(str){
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
